2f25eb6ebda843ce57528011fd567537025a9d05
annamaria99 <ams58@hi.is>
2018-10-30 19:03:46 +0000

    Klárað að mestu

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 
 // fasti sem segir til um hve marga leiki eigi að spila
 const GAMES_TO_PLAY = 10;
+let rett = 0;
 
 /**
  * Birtir upplýsingar um leik og eftir að notandi samþykkir spilar fyrsta leik
@@ -13,7 +14,11 @@ const GAMES_TO_PLAY = 10;
  * Eftir leik er notanda boðið að spila annan leik, ef ekki hættir forrit.
  */
 function start() {
-  villa;
+  alert('Markmiðið er að svara eins mörgum af 10 dæmum rétt eins hratt og mögulegt er.');
+  do{
+    play();
+  }
+  while(confirm("Spila annan leik?"));
 }
 
 /**
@@ -28,11 +33,32 @@ function start() {
  *
  */
 function play() {
+  let spila = true;
+
+   const seconds_start = new Date().getTime();
+
+   for(let i = 0; i<GAMES_TO_PLAY; i++){
+    let p = ask();
+    if(p === null){
+      spila = !spila;
+      break;
+    }
+  }
+
+ const seconds_end = new Date().getTime(); //Tími í lokin
+
+ const seconds = ((seconds_end - seconds_start)/1000).toFixed(2); //mismunur loka og upphafstíma
+
+ const avg = (rett/seconds).toFixed(2); //meðalfjöldi réttra spurninga á sekúndu
+
+ if(spila === true)
+  alert("Þú svaraðir " + rett + " af " + GAMES_TO_PLAY + " dæmum rétt á " + seconds + " sekúndum \n Meðalrétt svör á sekúndu eru " + avg);
+
 }
 
 /**
  * Spyr einnar spurningar og skilar upplýsingum um svar (mögulega með því að
- * nota true, false og null ef notandi hættir). Birtir notanda propmpt til að
+ * nota true, false og null ef notandi hættir). Birtir notanda prompt til að
  * svara í og túlkar svarið yfir í tölu.
  *
  * Mögulegar spurningar eru:
@@ -45,7 +71,60 @@ function play() {
  * Sniðugt væri að færa það að búa til spurningu í nýtt fall sem ask() kallar í.
  */
 function ask() {
+let r = Question(randomNumber(0,3));
+if(r == true){
+  rett++
 }
+if(r == null){
+  alert('Hætt í leik');
+  return null;
+}
+}
+
+function Question(random){
+
+  if(random == 0) {
+    let a = randomNumber(1,100);
+    let b = randomNumber(1,100);
+    const input = prompt('Hvað er ' + a + '+' + b + '?');
+    if(parseInt(input) === a+b)
+    return true;
+    if(input === null)
+    return null;
+  }
+
+  if(random == 1){
+    let a = randomNumber(1,100);
+    let b = randomNumber(1,100);
+    const input = prompt('Hvað er ' + a + '-' + b + '?');
+    if(parseInt(input) === a-b)
+    return true;
+    if(input === null)
+    return null;
+  }
+
+  if(random == 2){
+    let a = randomNumber(1,10);
+    let b = randomNumber(1,10);
+    const input = prompt('Hvað er ' + a + '*' + b + '?');
+    if(parseInt(input) === a*b)
+    return true;
+    if(input === null)
+    return null;
+  }
+
+  if(random == 3) {
+    let a = randomNumber(2,10);
+    let b = a*randomNumber(2,10);
+    const input = prompt('Hvað er ' + b + '/' + a + '?');
+    if(parseInt(input) === b/a)
+    return true;
+    if(input === null)
+    return null;
+  }
+
+}
+
 
 /**
  * Skilar tölu af handahófi á bilinu [min, max]
